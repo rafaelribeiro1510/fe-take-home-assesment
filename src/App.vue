1faa6b70b8a7487df6c6fcd3ca429f8dd70e6497
Rafael Ribeiro <45789887+rafaelribeiro1510@users.noreply.github.com>
2025-08-31 12:02:15 +0100

    dark mode switch

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,20 @@
 <script setup lang="ts">
 import { VueQueryDevtools } from '@tanstack/vue-query-devtools';
 import { RouterView } from 'vue-router';
+import { ref, watch } from 'vue';
+import { useTheme } from 'vuetify';
+
+const theme = useTheme();
+const darkMode = ref(theme.global.name.value === 'dark');
+
+const toggleTheme = () => {
+  theme.global.name.value = theme.global.name.value === 'light' ? 'dark' : 'light';
+};
+
+watch(theme.global.name, (newTheme) => {
+  darkMode.value = newTheme === 'dark';
+});
+
 </script>
 
 <template>
@@ -8,6 +22,14 @@ import { RouterView } from 'vue-router';
     <v-main>
       <v-app-bar>
           <v-app-bar-title text="Front-End Engineer Coding Exercise"></v-app-bar-title>
+          <v-app-bar-actions>
+              <v-switch 
+                v-model="darkMode" 
+                @change="toggleTheme()"
+                class="me-4"
+                false-icon="mdi-brightness-6"
+                true-icon="mdi-brightness-2" />
+          </v-app-bar-actions>
       </v-app-bar>
       <RouterView />
     </v-main>
